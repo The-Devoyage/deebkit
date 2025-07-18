b88f00ebcda540545c3466f1d0ae2f9f28c3b23f
Nick McLean <nickisyourfan@gmail.com>
2025-07-17 22:11:39 -0500

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-import nextra from 'nextra'
+import nextra from "nextra";
 
-const withNextra = nextra({})
+const withNextra = nextra({});
 
-export default withNextra({})
+export default withNextra({
+  output: "export",
+});
