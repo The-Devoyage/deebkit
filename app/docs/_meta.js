f55b83637ed1479b9cd74e94d8aa40872d0ad1b4
Nick McLean <nickisyourfan@gmail.com>
2025-07-29 22:05:07 -0500

    improve association docs

--- app/docs/_meta.js
+++ app/docs/_meta.js
@@ -5,6 +5,7 @@ const meta = {
   ["collection-trait"]: "The Collection Trait",
   ["programmatic-usage"]: "Programmatic Usage",
   ["queries"]: "Queries",
+  ["associations-and-enrichments"]: "Associations & Enrichments",
   ["paginate-and-sort"]: "Paginate and Sort",
   ["transactions"]: "Transactions",
 };
